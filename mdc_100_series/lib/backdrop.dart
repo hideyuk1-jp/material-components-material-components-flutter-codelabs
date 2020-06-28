1e72a88ef56f70cd1b3789aaacb6ed936a62d7f0
hideyuk1-jp <hh11028+github@gmail.com>
2020-06-28 15:13:09 +0900

    5. Add a shape

--- mdc_100_series/lib/backdrop.dart
+++ mdc_100_series/lib/backdrop.dart
@@ -29,6 +29,35 @@ class Backdrop extends StatefulWidget {
 }
 
 // TODO: Add _FrontLayer class (104)
+class _FrontLayer extends StatelessWidget {
+  // TODO: Add on-top callback (104)
+  const _FrontLayer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 16.0,
+      shape: BeveledRectangleBorder(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(46.0)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          // TODO: Add a GestureDetector (104)
+          Expanded(
+            child: child,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
 // TODO: Add _BackdropTitle class (104)
 // TODO: Add _BackdropState class (104)
 class _BackdropState extends State<Backdrop>
@@ -44,7 +73,9 @@ class _BackdropState extends State<Backdrop>
       children: <Widget>[
         // TODO: Wrap backLayer in an ExckudeSemantics widget (104)
         widget.backLayer,
-        widget.frontLayer,
+        // TODO: Add a PositionedTransition (104)
+        // TODO: Wrap front layer in _FrontLayer (104)
+        _FrontLayer(child: widget.frontLayer),
       ],
     );
   }
